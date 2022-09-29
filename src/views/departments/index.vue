4429d37d8544caa9e6d19a0ab95e163d70c84279
lin <2284309229@qq.com>
2022-09-29 14:08:28 +0800

    feat:组织架构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    departments
+    <el-card>
+      <el-row type="flex" justify="space-around" align="middle" style="height:40px">
+        <el-col>
+          <span>江苏传智播客教育科技股份有限公司</span>
+        </el-col>
+        <el-col :span="4">
+          <el-row type="flex">
+            <el-col>
+              <span>负责人</span>
+            </el-col>
+            <el-col>
+              <el-dropdown>
+                <span>
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
